f09d858c313faa3cf01e05b0bcf33325ed000589
liiinx <liiinx.com@gmail.com>
2022-11-15 18:58:27 +0000

    Update 5 files

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -20,4 +20,34 @@ export class BotService {
 
     return Promise.reject("NOT_SUPPORTED_MESSAGE_TYPE");
   }
+
+  async sendWhatsappResponse(senderPsid: string, response: any) {
+    // The page access token we have generated in your app settings
+    const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+    // Construct the message body
+    const requestBody = {
+      recipient: {
+        id: senderPsid,
+      },
+      message: response,
+    };
+
+    // Send the HTTP request to the Messenger Platform
+    request(
+      {
+        uri: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: "POST",
+        json: requestBody,
+      },
+      (err, _res, _body) => {
+        if (!err) {
+          console.log("Message sent!");
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      },
+    );
+  }
 }
