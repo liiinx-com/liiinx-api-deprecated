2f86967d47b35680872b5aca9efa00da55bdfa24
liiinx <liiinx.com@gmail.com>
2022-11-21 14:56:37 +0000

    Update menu.ts

--- src/bot/flows/menu.ts
+++ src/bot/flows/menu.ts
@@ -1,7 +1,12 @@
 const NEW_LINE = "\n";
 
 const randomIntBetween = ({start = 0, end=0}) => {
-    return 0
+    return 0;
+}
+
+const greetingMessage = ({name}, {randomizer}) => {
+    const repo = [`Hi ${name}!`]
+    const messageIndex = randomizer ? randomizer({end: repo.length}) : 0
     
 }
 
