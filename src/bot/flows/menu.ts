c76e9fcaac78cffc1fc5f902cdc41f6f2141d497
Amir Zad <azad@iiroc.ca>
2022-11-21 10:30:45 -0500

    improve menu

--- src/bot/flows/menu.ts
+++ src/bot/flows/menu.ts
@@ -1,16 +1,26 @@
 const NEW_LINE = "\n";
 
-const randomIntBetween = ({inclusiveMin= 0, exclusiveMax=0}) => {
-    
-     const minValue = Math.ceil(inclusiveMin);
+const randomIntBetween = ({ inclusiveMin = 0, exclusiveMax = 0 }) => {
+  const minValue = Math.ceil(inclusiveMin);
   const maxValue = Math.floor(exclusiveMax);
   return Math.floor(Math.random() * (maxValue - minValue) + minValue); // The maximum is exclusive and the minimum is inclusive
-}
+};
 
-const greetingMessage = ({name}, {randomizer}) => {
-    const repo = [`Hi ${name}!`]
-    const messageIndex = randomizer ? randomizer({exclusiveMax: repo.length}) : 0
-    
-}
+const bold = (text: string) => `**${text}**`;
+const italic = (text: string) => `*${text}*`;
 
-const mainMenu = async () => {};
+const greetingMessage = ({ name }, { randomFn = undefined }) => {
+  const repo = [`Hi ${name}!`];
+  const messageIndex = randomFn ? randomFn({ exclusiveMax: repo.length }) : 0;
+  return repo[messageIndex];
+};
+
+const mainMenu = async ({ name }) => {
+  const result = [];
+  result.push(greetingMessage({ name }, { randomFn: randomIntBetween }));
+  result.push(NEW_LINE);
+  return result;
+};
+
+const params = { name: "Amir" };
+console.log(mainMenu(params));
