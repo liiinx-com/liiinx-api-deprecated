339bf2a008f40141382d3a8d6f07f1cbed434e1b
liiinx <liiinx.com@gmail.com>
2022-11-21 15:00:25 +0000

    Update menu.ts

--- src/bot/flows/menu.ts
+++ src/bot/flows/menu.ts
@@ -1,12 +1,15 @@
 const NEW_LINE = "\n";
 
-const randomIntBetween = ({start = 0, end=0}) => {
-    return 0;
+const randomIntBetween = ({inclusiveMin = 0, exclusiveMax=0}) => {
+    
+     const minValue = Math.ceil(inclusiveMin);
+  const maxValue = Math.floor(exclusiveMax);
+  return Math.floor(Math.random() * (maxValue - minValue) + minValue); // The maximum is exclusive and the minimum is inclusive
 }
 
 const greetingMessage = ({name}, {randomizer}) => {
     const repo = [`Hi ${name}!`]
-    const messageIndex = randomizer ? randomizer({end: repo.length}) : 0
+    const messageIndex = randomizer ? randomizer({exclusiveMax: repo.length}) : 0
     
 }
 
