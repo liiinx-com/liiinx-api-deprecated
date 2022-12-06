57515da37cfab7eb53f40e128dc5c2ae016b54df
liiinx <liiinx.com@gmail.com>
2022-12-06 02:43:43 +0000

    Update bot.service.ts

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,6 +1,6 @@
-import { firstValueFrom } from "rxjs";
+import { catchError, firstValueFrom } from "rxjs";
 import { HttpService } from "@nestjs/axios";
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { IncomingMessage } from "./bot.type";
 import assets from "./assets";
 import { IntentService } from "./bot-intent.service";
@@ -8,9 +8,98 @@ import { IntentManager } from "./intent-manager";
 
 const TOKEN = "";
 
+const listInteractiveObject = {
+  type: "list",
+  header: {
+    type: "text",
+    text: "Select the food item you would like",
+  },
+  body: {
+    text: "You will be presented with a list of options to choose from",
+  },
+  footer: {
+    text: "All of them are freshly packed",
+  },
+  action: {
+    button: "Order",
+    sections: [
+      {
+        title: "Section 1 - Fruit",
+        rows: [
+          {
+            id: "1",
+            title: "Apple",
+            description: "Dozen",
+          },
+          {
+            id: "2",
+            title: "Orange",
+            description: "Dozen",
+          },
+        ],
+      },
+      {
+        title: "Section 2 - Vegetables",
+        rows: [
+          {
+            id: "3",
+            title: "Spinach",
+            description: "1kg ",
+          },
+          {
+            id: "2",
+            title: "Broccoli",
+            description: "1kg",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const sampleInteractiveMsg = ({ to }) => ({
+  to,
+  type: "interactive",
+  interactive: {
+    type: "button",
+    body: {
+      text: "optional body text",
+    },
+    footer: {
+      text: "optional footer text",
+    },
+    action: {
+      buttons: [
+        {
+          type: "reply",
+          reply: {
+            id: "BUTTON_ID_1",
+            title: "TITLE_1",
+          },
+        },
+        {
+          type: "reply",
+          reply: {
+            id: "BUTTON_ID_3",
+            title: "TITLE_3",
+          },
+        },
+        {
+          type: "reply",
+          reply: {
+            id: "BUTTON_ID_2",
+            title: "TITLE_2",
+          },
+        },
+      ],
+    },
+  },
+});
+
 @Injectable()
 export class BotService {
   private static baseUrl = "https://graph.facebook.com/v15.0/";
+  private readonly logger = new Logger(BotService.name);
 
   constructor(
     private readonly intentManager: IntentManager,
@@ -56,7 +145,15 @@ export class BotService {
     text,
     replyingMessageId = null,
     previewUrl = false,
+    interactiveMessage = true,
   }): any {
+    if (interactiveMessage)
+      return {
+        to,
+        type: "interactive",
+        interactive: listInteractiveObject,
+      };
+
     const result: any = {
       type: "text",
       to,
@@ -70,7 +167,8 @@ export class BotService {
         preview_url: previewUrl,
       },
     };
-    if (replyingMessageId) result.context = { message_id: replyingMessageId };
+    if (replyingMessageId && false)
+      result.context = { message_id: replyingMessageId };
     return result;
   }
 
@@ -91,10 +189,10 @@ export class BotService {
       return this.send(
         responses.map(
           (r: any) =>
-            ({
+            new WhatsappResponse({
               data: r,
               phoneNumberId,
-            } as WhatsappResponse),
+            }),
         ),
       );
     }
@@ -125,20 +223,34 @@ export class BotService {
       };
 
       console.log("sending => ", JSON.stringify(updatedPayload, null, 2));
-      result.push(
-        firstValueFrom(
-          this.http.post(this.getUrl({ phoneNumberId }), updatedPayload, {
+
+      const { data: responseData } = await firstValueFrom(
+        this.http
+          .post(this.getUrl({ phoneNumberId }), updatedPayload, {
             headers,
-          }),
-        ),
+          })
+          .pipe(
+            catchError((error: any) => {
+              this.logger.error(error.response.data);
+              throw "An error happened!";
+            }),
+          ),
       );
+      result.push(responseData);
     }
+
     return result;
   }
 }
 
 export class WhatsappResponse {
   data: object;
-  recipient_type?: string = "individual";
+  recipient_type: string;
   phoneNumberId: string;
+  constructor(params) {
+    const { data, phoneNumberId } = params;
+    this.data = data;
+    this.phoneNumberId = phoneNumberId;
+    this.recipient_type = "individual";
+  }
 }
